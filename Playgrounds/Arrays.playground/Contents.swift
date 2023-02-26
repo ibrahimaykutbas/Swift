d3c5216c2e4c24c2fa25997097cf805248ff080a
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2023-02-26 14:18:37 +0300

    Add Playgorunds

---
+++ Playgrounds/Arrays.playground/Contents.swift
@@ -0,0 +1,3 @@
+import UIKit
+
+var myFavoriteMovies  = ["Matrix","LOTR","Star Wars","Harry Potter",5,true] as [Any]
---
+++ Playgrounds/Variables.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//Variables & Constants
+
+// snake_case
+// camelCase
+
+var userName = "İbrahim Aykut"
+userName.append("O")
+userName.lowercased()
+userName.uppercased()
+var userSurname = "BAŞ"
+
+userSurname = "BAS"
+
+let userAge = 26
+
+// Tip Belirtmeler
+var myString : String = "İbo Wan Kenobi"
+var myNumber : Int = 10
+
+// Tip dönüşümü
+let stringNumber = String(20)
+
+
+let myVariable : String //define
+myVariable = "Test" //initialization
+myVariable.uppercased()
